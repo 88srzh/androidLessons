81b641fa58e44275c152f2eab4f636a68e9d4552
Sergey Petrov <smpetrov18@yandex.ru>
2020-09-12 00:20:17 +0400

    checkbox add

--- p0091_OnClickButtons/src/main/java/com/example/p0091_onclickbuttons/MainActivity.java
+++ p0091_OnClickButtons/src/main/java/com/example/p0091_onclickbuttons/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.p0091_onclickbuttons;
 
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,14 +15,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button button2;
     Button button3;
 
+    private static final String TAG = "myLogs";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG,"найдем view - элементы");
         textView = (TextView) findViewById(R.id.textView);
-        button1 = (Button) findViewById(R.id.button);
+        button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
 
@@ -29,6 +33,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(View view) {
                 textView.setText("Нажата кнопка 1");
+                Log.d(TAG, "обработаем нажатие кнопки 1");
+
+                Toast toast = Toast.makeText(MainActivity.this, "Нажата кнопка 1", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                LinearLayout toastImage = (LinearLayout) toast.getView();
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(R.drawable.baseline_done_black_18dp);
+                toastImage.addView(imageView, 0);
+                toast.show();
             }
         });
 
@@ -38,10 +51,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void clickButton3(View view) {
         textView.setText("Нажата кнопка 3");
+        Log.d(TAG, "обработаем нажатие кнопки 3");
     }
 
     @Override
     public void onClick(View view) {
         textView.setText("Нажата кнопка 2");
+        Log.d(TAG, "обработаем нажатие кнопки 2");
     }
 }
