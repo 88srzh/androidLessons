c9701b4993baf9d0b097d93350b0f1871c02bfe0
srzh <smpetrov18@yandex.ru>
2020-09-14 10:24:35 +0400

    091

--- p0091_OnClickButtons/src/main/java/com/example/p0091_onclickbuttons/MainActivity.java
+++ p0091_OnClickButtons/src/main/java/com/example/p0091_onclickbuttons/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.p0091_onclickbuttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button button1;
     Button button2;
     Button button3;
+
+    private static final String TAG = "myLog";
 
 
     @Override
@@ -25,10 +28,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
 
+        Log.d(TAG, "найдем View - элементы");
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 textView.setText("Нажата кнопка 1");
+                Log.d(TAG, "Обработаем нажатие кнопки 1");
             }
         });
 
@@ -38,10 +44,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void clickButton3(View view) {
         textView.setText("Нажата кнопка 3");
+        Log.d(TAG, "Обработаем нажатие кнопки 3");
     }
 
     @Override
     public void onClick(View view) {
         textView.setText("Нажата кнопка 2");
+        Log.d(TAG, "Обработаем нажатие кнопки 2");
     }
 }
